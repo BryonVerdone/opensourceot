1d5798a9bab675721b80b1aa84666683588bb347
BryonVerdone <bryonverdone@gmail.com>
2022-07-05 19:17:13 -0400

    added simple styles to nav and footer comps

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-const Navbar = () => {
+import './index.css';
+import logo from '../../assets/logo.png';
+const Navigation = () => {
   return (
-    <nav>
-      <NavLink to='/'>Home</NavLink>
-      <NavLink to='about'>About</NavLink>
-      <NavLink to='activities'>Activities</NavLink>
+    <nav className='navbar'>
+      <img className='logo' src={logo} alt='' />
+      <div className='links-container'>
+        <NavLink to='/'>Home</NavLink>
+        <NavLink to='about'>About</NavLink>
+        <NavLink to='activities'>Activities</NavLink>
+      </div>
     </nav>
   );
 };
 
-export default Navbar;
+export default Navigation;

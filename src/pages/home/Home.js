7dfd2684adf386822c459b3939c87ecd839c5c16
BryonVerdone <bryonverdone@gmail.com>
2022-07-09 17:53:44 -0400

    added responsive hero section

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,10 +1,28 @@
 import React from 'react';
-
+import './index.css';
+import heroImage from '../../assets/hero-img.png';
 const Home = () => {
   return (
-    <div>
-      <h1 style={{ textAlign: 'center' }}>Home Section</h1>
-    </div>
+    <>
+      <section className='hero-section'>
+        <div className='hero-container'>
+          <div className='hero-text'>
+            <h1>Occupational Therapy Activities From The OT Community</h1>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit ut
+              aliquam, purus sit amet luctu
+            </p>
+            <div className='hero-btn-container'>
+              <button className='btn'>Activities</button>
+              <button className='btn'>About</button>
+            </div>
+          </div>
+          <div className='hero-img-container'>
+            <img className='hero-img' src={heroImage} alt='' />
+          </div>
+        </div>
+      </section>
+    </>
   );
 };
 

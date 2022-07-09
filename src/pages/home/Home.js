a27b4b9d07f70f4910737094dfea2227f61863bd
BryonVerdone <bryonverdone@gmail.com>
2022-07-09 19:11:16 -0400

    added responsive goal section

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './index.css';
 import heroImage from '../../assets/hero-img.png';
+import goalImage from '../../assets/goal-img.png';
 const Home = () => {
   return (
     <>
@@ -18,7 +19,33 @@ const Home = () => {
             </div>
           </div>
           <div className='hero-img-container'>
-            <img className='hero-img' src={heroImage} alt='' />
+            <img
+              className='hero-img'
+              src={heroImage}
+              alt='child reading book'
+            />
+          </div>
+        </div>
+      </section>
+      <section className='goal-section'>
+        <div className='goal-container'>
+          <div className='goal-text'>
+            <h2>Our Goal</h2>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit ut
+              aliquam, purus sit amet luctus venenatis, lectus magna fringilla
+              urna, porttitor rhoncus dolor purus non enim praesent elementum
+              facilisis leo, vel fringilla est ullamcorper eget nulla facilisi
+              etiam dignissim diam quis purus sit amet luctus venenatis, lectus
+              magna fringilla urna.
+            </p>
+          </div>
+          <div className='goal-img-container'>
+            <img
+              style={{ width: '400px' }}
+              src={goalImage}
+              alt='kids writing'
+            />
           </div>
         </div>
       </section>
